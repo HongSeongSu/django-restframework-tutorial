c3cb9573efb53de26bb158aa59ce8a319f687db2
sshong1991 <sshong1991@gmail.com>
2018-05-31 16:12:53 +0900

    tuto3 using mixins

---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import function_views, class_views
+
+# urlpatterns = [
+#     url(r'^snippets/$', function_views.snippet_list),
+#     url(r'^snippets/(?P<pk>[0-9]+)/$', function_views.snippet_detail),
+#
+# ]
+
+urlpatterns = [
+    url(r'^snippets/$', class_views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', class_views.SnippetDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
