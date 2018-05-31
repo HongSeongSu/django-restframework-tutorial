6662b981cfd712e6134111440006856c34d15e29
sshong1991 <sshong1991@gmail.com>
2018-05-31 17:00:23 +0900

    tuto4 Adding endpoints for our User models

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -11,6 +11,8 @@ from snippets import function_views, class_views
 urlpatterns = [
     url(r'^snippets/$', class_views.SnippetList.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)/$', class_views.SnippetDetail.as_view()),
+    url(r'^users/$', class_views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', class_views.UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
