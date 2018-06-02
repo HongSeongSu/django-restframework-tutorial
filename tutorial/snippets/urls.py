040557edf54d908ffa79235e7b8ceaa4cae61571
sshong1991 <sshong1991@gmail.com>
2018-06-02 17:07:46 +0900

    Making sure our URL patterns are named

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -8,11 +8,11 @@ from snippets import function_views, class_views
 #
 # ]
 
-urlpatterns = [
-    url(r'^snippets/$', class_views.SnippetList.as_view()),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', class_views.SnippetDetail.as_view()),
-    url(r'^users/$', class_views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', class_views.UserDetail.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns([
+    url(r'^$', class_views.api_root),
+    url(r'^snippets/$', class_views.SnippetList.as_view(), name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', class_views.SnippetDetail.as_view(), name='snippet-detail'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', class_views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    url(r'^users/$', class_views.UserList.as_view(), name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', class_views.UserDetail.as_view(), name='user-detail')
+])
