a0a6d8812330b22774130df8792a009dba084e8f
sshong1991 <sshong1991@gmail.com>
2018-06-02 18:08:23 +0900

    Binding ViewSets to URLs explicitly

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from snippets import function_views, class_views
+from snippets import function_views
+from snippets.class_views import SnippetViewSet, UserViewSet, api_root
+from rest_framework import renderers
 
 # urlpatterns = [
 #     url(r'^snippets/$', function_views.snippet_list),
@@ -8,11 +10,31 @@ from snippets import function_views, class_views
 #
 # ]
 
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
+
 urlpatterns = format_suffix_patterns([
-    url(r'^$', class_views.api_root),
-    url(r'^snippets/$', class_views.SnippetList.as_view(), name='snippet-list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', class_views.SnippetDetail.as_view(), name='snippet-detail'),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', class_views.SnippetHighlight.as_view(), name='snippet-highlight'),
-    url(r'^users/$', class_views.UserList.as_view(), name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', class_views.UserDetail.as_view(), name='user-detail')
+    url(r'^$', api_root),
+    url(r'^snippets/$', snippet_list, name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail, name='snippet-detail'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', snippet_highlight, name='snippet-highlight'),
+    url(r'^users/$', user_list, name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail')
 ])
