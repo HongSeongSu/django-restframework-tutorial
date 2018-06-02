3e7fa8ed228cc15964e22eade184dd755e2816e9
sshong1991 <sshong1991@gmail.com>
2018-06-02 18:43:53 +0900

    Adding a schema

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from snippets import class_views
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
+
 
 router = DefaultRouter()
 router.register(r'snippets', class_views.SnippetViewSet)
 router.register(r'users', class_views.UserViewSet)
 
 urlpatterns = [
+    url(r'^schema/$', schema_view),
     url(r'^', include(router.urls))
 ]
